f44a3ad1a7d98668add3e2cbae26f90c851ac233
Eric Bannon <eric.bannon4@gmai.com>
2019-09-23 14:35:29 -0700

    Adding new script

--- prometheus-setup.sh
+++ prometheus-setup.sh
@@ -13,6 +13,14 @@ sleep 1
 echo "adding incubator charts repo"
 
 helm repo add incubator http://storage.googleapis.com/kubernetes-charts-incubator
+helm init
+helm update
+
+sleep 1
+
+echo "Creating developers namespace"
+
+kubectl create namespace developers
 
 echo "Deploying default storage class for Kafka"
 
@@ -20,7 +28,7 @@ kubectl create -f https://raw.githubusercontent.com/platform9/prometheus-kafka/m
 
 echo "Deploying Kafka..."
 
-helm install --name kafka --set prometheus.jmx.enabled=true,prometheus.kafka.enabled=true,persistence.size=40Gi incubator/kafka
+helm install --namespace developers --name kafka --set prometheus.jmx.enabled=true,prometheus.kafka.enabled=true,persistence.size=40Gi incubator/kafka
 
 echo "Waiting several seconds..."
 sleep 3
@@ -37,13 +45,32 @@ echo "Deploying Service Monitor for kafka-prometheus metrics..."
 
 kubectl create -f https://raw.githubusercontent.com/platform9/prometheus-kafka/master/kafka-svc-mon.yaml
 
-echo "Exposing Kafka SM service..."
-
-kubectl create -f https://raw.githubusercontent.com/platform9/prometheus-kafka/master/kafka-sm-service.yaml
-
 echo "Deploying Grafana via helm chart..."
 
 kubectl create -f https://raw.githubusercontent.com/platform9/prometheus-kafka/master/grafana.yaml
 
-echo "Done!"
+sleep 1
+
+echo "Exposing grafana UI over cloud loadbalancer..."
+
+sleep 1
+
+kubectl expose deployment grafana -n developers --type=LoadBalancer --port=9000--target-port=9000
+
+echo "Exposing Prometheus over cloud loadbalancer to verify kafka targets under service discovery in Prometheus UI"
+
+sleep 1
+
+kubectl expose prometheus kafka-watcher -n developers --type=LoadBalancer --port=9090 --target-port=9090
+
+sleep 1
+
+kubectl get services -n developers
+
+echo "Go to the Prometheus UI and confirm targets are coming through under 'Service Discovery'"
+
+sleep 1
+
+echo "...Proceed to the Grafana UI and add the prometheus external IP to the targets for data discovery!"
+
 fi
